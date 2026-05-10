1d79b03828fbbe0f282e68c2ccb9d0856edc04de
Anirudh-Saini10 <markwilsonx2020@gmail.com>
2026-05-10 20:55:45 +0530

    fix gemini utils and update requirements

--- gemini_utils.py
+++ gemini_utils.py
@@ -1,20 +1,17 @@
-import google.generativeai as genai
+from google import genai
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-model = genai.GenerativeModel("models/gemini-flash-latest")
-
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 def get_gemini_response(prompt):
-
     try:
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=prompt
+        )
         return response.text
-
     except Exception as e:
         return str(e)
